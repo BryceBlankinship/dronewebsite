2bf1f51ed94b89f0401cad20d7d43aa00b7d8ba7
Bryce Blankinship <blankinshipbusiness@gmail.com>
2022-05-02 22:36:57 -0400

    hero v1

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './hero.module.css';
+import video from './assets/hero.mp4';
 
-export default function Hero(){
-    return(
-      <div className={styles.heroContainer}>
-          Cinematix
+export default function Hero() {
+  return (
+    <div className={styles.heroContainer}>
+      <div className={styles.heroVideo}>
+        <video src={video} autoPlay muted loop></video>
       </div>
-    );
-  }
+      <div className={styles.heroHeader}>
+        <h1>Cinematix</h1>
+        <h3>Commercial Cinematography</h3>
+      </div>
+    </div>
+  );
+}
